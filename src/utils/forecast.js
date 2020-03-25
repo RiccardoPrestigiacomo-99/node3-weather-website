f42de5b3e5402523da3d0cd5cc628aabeb21b361
RiccardoPoli99 <riccardo.prestigiacomo@gmail.com>
2020-03-25 19:03:26 +0100

    Add wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,11 @@
 const request = require('request')
 
+
+
+
+
+
+
 const forecast = (longitude, latitude, callback) => {
     
     const url = 'https://api.darksky.net/forecast/e8328fe62a0034a7741cf9958842d54d/' + latitude + ',' + longitude
@@ -11,7 +17,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined,body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degress out. There is a '
-            + body.currently.precipProbability + '% chance of rain.'
+            + body.currently.precipProbability + '% chance of rain. Wind speed is: ' + body.currently.windSpeed
             )
         }
     })
